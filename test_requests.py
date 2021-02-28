b520c695b406a34bb470bd458dfbe362242ed3b9
Bananator <aloev.arun@gmail.com>
2021-02-28 22:48:54 +0300

    lil_changes

--- test_requests.py
+++ test_requests.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def read_data():
+def read_data():  # собираем иноформацию
     login = 'ArunAloev'  # str(input('Login'))
     password = '0512'  # str(input('Password:'))
     ip = '172.19.3.61'  # str(input('IP:'))
@@ -12,7 +12,7 @@ def read_data():
     return login, password, ip, port, x, y, z
 
 
-def cam_set_pos(in_log, in_pass, in_ip, in_port, in_x, in_y, in_z):
+def cam_set_pos(in_log, in_pass, in_ip, in_port, in_x, in_y, in_z):  # наведение лазера смерти через http запрос
     url = f'http://{in_log}:{in_pass}@{in_ip}:{in_port}/\
                                                      execute?cameraID=3&action=setposition&x={in_x}&y={in_y}&z={in_z}'
     payload = {}

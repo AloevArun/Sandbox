19d5266bd9ace5e7e08e984a5b61c633699a24fc
ipdroom <arun.aloev@gmail.com>
2021-03-02 13:04:37 +0300

    Try some Class

---
+++ web2_requests/do_some.py
@@ -0,0 +1,1 @@
+import web2_requests
---
+++ web2_requests/web2_requests.py
@@ -0,0 +1,46 @@
+import web2_requests
+import json
+import socket
+
+
+class UserData:
+    def __init__(self):
+        self.ports = dict(web1=80, web2=8085, auto_rest=8091, rest_ip='127.0.0.1')
+        self.current_ip = socket.gethostbyname(socket.gethostname())
+        self.current_host = socket.gethostname()
+        self.auth = ('user', 'pass')
+        self.host = f'{self.current_ip}:{self.ports.get("port")}'
+
+    def set_data(self):  # собираем иноформацию
+        pass
+
+    def get_data(self):
+        return self.auth, self.host
+
+
+class Web2Request(UserData):
+    def get_all_objects_on_server(self, auth, host):
+        self.url = f'http://{host}/web2/secure/configuration'
+        payload = {}
+        headers = {}
+        response = web2_requests.request("GET", url, headers=headers, data=payload, auth=auth)
+        parsed = json.loads(response.content)
+        for dict_n in parsed:
+            print('\n')
+            for item in dict_n.items():
+                print(item)
+
+
+def set_xyz(self):
+    x = str(140)  # str(input('X:'))
+    y = str(-100)  # str(input('Y:'))
+    z = str(50)  # str(input('Zoom:'))
+    return x, y, z
+
+
+def cam_set_pos(self, auth, host, x, y, z):  # наведение лазера смерти через http запрос
+    url = f'http://{host}/execute?cameraID=3&action=setposition&x={x}&y={y}&z={z}'
+    payload = {}
+    headers = {}
+    response = web2_requests.request("GET", url, headers=headers, data=payload, auth=auth)
+    print(response)

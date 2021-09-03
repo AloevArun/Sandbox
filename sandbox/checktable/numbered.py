46e2ff2f469d97d1034d6fad0b7609341bf04405
Bananator <aloev.arun@gmail.com>
2021-09-03 22:34:58 +0300

    checkpoint

---
+++ sandbox/checktable/numbered.py
@@ -0,0 +1,12 @@
+def numbered(x):
+    row, numbers, cells, size = '', '', ['▓', '░'], len(str(x))
+    row_size = round((size + 1) / 2)
+    for i in range(0, x):
+        row += cells[i % 2] * size
+        numbers += ' ' * (size - len(str(i + 1))) + str(i + 1)
+    print(numbers)
+    for j in range(0, row_size):
+        print(row)
+
+
+numbered(9999)
